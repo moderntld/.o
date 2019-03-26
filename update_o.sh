c3f66041efe82bb47457ef8b2ece1c1efe0dd963
Jonah Aragon <jonah@triplebit.net>
2019-03-25 19:06:44 -0500

    Add ns9

--- update_o.sh
+++ update_o.sh
@@ -29,6 +29,7 @@ git reset --hard origin/master > /dev/null
 # ADD NAMESERVERS!
 { echo "; TLD information"
   echo "		IN	NS	ns11.opennic.glue."
+  echo "		IN	NS	ns9.opennic.glue."
   echo "		IN	NS	ns2.opennic.glue."
   echo "		IN	NS	ns6.opennic.glue."
   echo "		IN	NS	ns8.opennic.glue."
